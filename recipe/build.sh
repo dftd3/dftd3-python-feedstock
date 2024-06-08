c1286445ec761a0e54fd87af3aaee186fd2c45d0
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2024-06-08 14:21:38 +0200

    Update for installation with Python package metadata (#21)
    MNT: Re-rendered with conda-build 24.5.1, conda-smithy 3.36.1, and conda-forge-pinning 2024.06.07.13.04.51

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,25 +2,27 @@
 
 set -ex
 
-pushd python
-
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" != "1" ]]; then
-  MESON_ARGS=${MESON_ARGS:---prefix=${PREFIX} --libdir=lib}
-else
-  cat > pkgconfig.ini <<EOF
+cat > ${SRC_DIR}/pkgconfig.ini <<EOF
 [binaries]
 pkgconfig = '$BUILD_PREFIX/bin/pkg-config'
 EOF
-  MESON_ARGS="${MESON_ARGS:---prefix=${PREFIX} --libdir=lib} --cross-file pkgconfig.ini"
+
+build_args=(
+  "-Csetup-args=--warnlevel=0"
+  "-Csetup-args=--cross-file=${SRC_DIR}/pkgconfig.ini"
+  "-Csetup-args=-Dpython_version=${PREFIX}/bin/python3"
+)
+if [ -f ${BUILD_PREFIX}/meson_cross_file.txt ]; then
+  build_args=("${build_args[@]}" "-Csetup-args=--cross-file=${BUILD_PREFIX}/meson_cross_file.txt")
 fi
 
-meson setup _build \
-  ${MESON_ARGS} \
-  --buildtype=release \
-  --warnlevel=0 \
-  -Dpython_version=$PYTHON
+mv python/mesonpep517.toml python/pyproject.toml
 
-meson compile -C _build
-meson install -C _build --no-rebuild
+${PYTHON} -m build \
+    "${build_args[@]}" \
+    --outdir . \
+    --no-isolation \
+    --wheel \
+    python/
 
-popd
+${PYTHON} -m pip install *.whl --no-deps -vv
